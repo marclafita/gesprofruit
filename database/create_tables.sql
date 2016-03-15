9bad42c9014c28511c43b9ef72db0f793973f5aa
Marc Lafita Forcada <marclafita@gmail.com>
2016-03-15 21:37:55 +0100

    Creació de la secuencia per la taula d'usuaris

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -6,3 +6,15 @@ CREATE TABLE gesprofruit.usuaris (
    ultima_conexio DATE NOT NULL,   
   PRIMARY KEY (idusuari))
   TABLESPACE tbs_gesprofruit;
+  
+CREATE SEQUENCE seq_usuaris_idusuari
+	START WITH 1 
+	INCREMENT BY 1 
+	NOMAXVALUE; 	
+
+CREATE TRIGGER trig_usuaris_seq
+  BEFORE INSERT ON gesprofruit.usuaris
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_usuaris_idusuari.nextval INTO :new.idusuari FROM dual;
+  END;
